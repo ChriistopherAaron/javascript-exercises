befb790c87dd138638ba7b9eccb7fd43226f5b00
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-11 01:22:47 -0700

    Complete 04_sumAll exercise

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+// Check for non-integer types, non-numbers, or negative values
+if (!Number.isInteger(min) || !Number.isInteger(max)) return `ERROR`;
+if (min < 0 || max < 0) return `ERROR`;
 
+// DSwap parameters if larger number comes first (e.g. sumAll(123, 1))
+if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+}
+
+let sum = 0;
+for (let i = min; i <= max; i++) {
+    sum +=i;
+}
+
+return sum;
 };
 
 // Do not edit below this line
