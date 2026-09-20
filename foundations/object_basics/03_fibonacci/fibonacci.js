8c24a7af4c629792c58aa294477eab0b6ad6bb50
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-19 20:43:59 -0700

    Complete 03_fibonacci

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    const target = Number(count);
+    if (target < 0) {
+    return "OOPS";}
+    let a = 0;
+    let b = 1;
+for ( let i = 0; i < target; i++) {
+    const temp = a + b;
+    a = b;
+    b = temp;
+}
+return a;
 };
 
 // Do not edit below this line
