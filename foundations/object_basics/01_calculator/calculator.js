a26f10f7fa6a9bf109d77de59c88bc339f8486aa
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-19 19:56:29 -0700

    Complete 01_calculator exercise

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(num1, num2) {
+  
+  return(num1 + num2);
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (const num of array) {
+    total +=num;
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
+for (const num of array) {
+  total *=num;
+} 
+return total;
 
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+
+	for (let i = n; i > 1; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
